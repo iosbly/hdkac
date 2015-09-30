84b1a44572eea883572f2af2cf1711a3de48aca6
iosbly <io.sbly@gmail.com>
2015-09-30 16:57:54 +0200

    Validation

---
+++ hdkac/index.php
@@ -0,0 +1,37 @@
+<html>
+	<?php include('inc/header.php'); ?>
+	<body>
+		<div id="header1">
+
+		</div>
+
+		<div id="barre_recherche">
+			<div class="contenue">
+				<div class="main clearfix">
+					<div class="colonne">
+						<div id="recherche" class="recherche">
+							<form>
+								<input class="recherche-input" placeholder="tapez votre recherche..." type="text" value="" name="recherche" id="recherche">
+								<input class="recherche-submit" type="submit" value="">
+								<span class="icon-search"></span>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+		<center>
+		<img class="gif_container__animated gif_animated--89003" alt="" src="https://s.qwant.com/thumbr/?u=http%3A%2F%2Fimg.pr0gramm.com%2F2014%2F09%2F14%2F7f5f701e50cbb66a.gif" style="display: inline-block;">
+		</center>
+		<!-- /contenue -->
+		<script src="js/classie.js"></script>
+		<script src="js/uisearch.js"></script>
+		<script>
+			new UISearch( document.getElementById( 'recherche' ) );
+		</script>
+			</div>
+		</div>
+
+	</body>
+
+</html>
