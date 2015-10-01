7dca33261ef636c798c2463e47741690bfdaf072
iosbly <io.sbly@gmail.com>
2015-10-01 09:32:22 +0200

    Mis à jour

--- inc/header.php
+++ inc/header.php
@@ -1,10 +1,9 @@
 <head>
 		<title>HDKAC</title>
-		<link rel="stylesheet" type="text/css" href="http://localhost/hdkac/style1.css"/> <!-- Feuille de style globale -->
+		<link rel="stylesheet" type="text/css" href="http://localhost/hdkacgit/css/style1.css"/> <!-- Feuille de style globale -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" media="screen" type="text/css" href="../../css/style_pop.css"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<link rel="stylesheet" type="text/css" href="css/default.css" />
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="http://localhost/hdkacgit/css/default.css" />
+		<link rel="stylesheet" type="text/css" href="http://localhost/hdkacgit/css/component.css" />
 		<script src="js/modernizr.custom.js"></script>
 	</head>
--- index.php
+++ index.php
@@ -4,30 +4,30 @@
 		<div id="header1">
 
 		</div>
-
 		<div id="barre_recherche">
-			<div class="contenue">
-				<div class="main clearfix">
-					<div class="colonne">
-						<div id="recherche" class="recherche">
-							<form>
-								<input class="recherche-input" placeholder="tapez votre recherche..." type="text" value="" name="recherche" id="recherche">
-								<input class="recherche-submit" type="submit" value="">
-								<span class="icon-search"></span>
-							</form>
+			<div class="container">
+					<div class="main clearfix">
+						<div class="column">
+							<div id="sb-search" class="sb-search">
+								<form>
+									<input class="sb-search-input" placeholder="tapez votre recherche..." type="text" value="" name="search" id="search">
+									<input class="sb-search-submit" type="submit" value="">
+									<span class="sb-icon-search"></span>
+								</form>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>	
 		<center>
+		<br>
 		<img class="gif_container__animated gif_animated--89003" alt="" src="https://s.qwant.com/thumbr/?u=http%3A%2F%2Fimg.pr0gramm.com%2F2014%2F09%2F14%2F7f5f701e50cbb66a.gif" style="display: inline-block;">
 		</center>
 		<!-- /contenue -->
-		<script src="js/classie.js"></script>
-		<script src="js/uisearch.js"></script>
+		<script src="http://localhost/hdkacgit/js/classie.js"></script>
+		<script src="http://localhost/hdkacgit/js/uisearch.js"></script>
 		<script>
-			new UISearch( document.getElementById( 'recherche' ) );
+			new UISearch( document.getElementById( 'sb-search' ) );
 		</script>
 			</div>
 		</div>
